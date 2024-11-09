55f2fe7f70ef7934957fcfdbbfc5577f4e8415a8
IsaiGp96 <abraham_gp96@outlook.com>
2024-11-08 20:43:02 -0600

    Actualización gitignore #4

---
+++ Consumo/Consumo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.WebServiceSoapClient cliente = new ServiceReference1.WebServiceSoapClient();
+            /*Ejercicio 1*/
+            /*string result = cliente.HelloWorld();
+            
+            Console.WriteLine(result);*/
+
+            /*Ejercicio 2*/
+            /*Console.WriteLine("Introduce tu nombre");
+            
+            string nombre = Console.ReadLine();
+
+            string results = cliente.Saludo(nombre);
+
+            Console.WriteLine(results);*/
+
+            /*Ejercicio 3*/
+            /*Console.WriteLine("Introduce tu mensaje");
+            
+            string mensaje = Console.ReadLine();
+
+            string results2 = cliente.GuardarLog(mensaje);
+
+            Console.WriteLine(results2);*/
+
+            /*Ejercicio 4*/
+            Console.WriteLine("Introduce dos valores");
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = cliente.Sumar(num1, num2);
+            Console.WriteLine("El resultado de la operación es: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Servicio/Servicio/WebService.asmx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Servicio
+{
+    /// <summary>
+    /// Descripción breve de WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public string Saludo(string nombre)
+        {
+            return "Hola " + nombre;
+        }
+        [WebMethod]
+        public string GuardarLog(string mensaje)
+        {
+            Funciones.Logs("LogsServicios", mensaje);
+            return "Ok";
+        }
+
+        [WebMethod]
+        public int Sumar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+
+    }
+}
